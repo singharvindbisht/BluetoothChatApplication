f7fafd95aa4cd2bc49ecdcfa53af0ec65c50600a
arvind singh <arvind.singh7112000@gmail.com>
2021-02-24 11:09:21 +0530

    enabling bluetooth

--- app/src/main/java/com/example/bluetoothchat/MainActivity.java
+++ app/src/main/java/com/example/bluetoothchat/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bluetoothchat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,10 +11,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothAdapter bluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBluetooth();
+    }
+
+    private void initBluetooth(){
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter==null)
+        {
+            Toast.makeText(this, "Bluetooth not found", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -25,14 +37,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()){
-            case R.id.search_bluetooth:
-                Toast.makeText(this, "Bluetooth Search Pressed...", Toast.LENGTH_SHORT).show();
+            case R.id.add_person:
+                Toast.makeText(this, "Add Person...", Toast.LENGTH_SHORT).show();
                 return true;
 
             case R.id.menu_enabled_bluetooth:
-                Toast.makeText(this, "Bluetooth Enabled Pressed...", Toast.LENGTH_SHORT).show();
+                enableBluetooth();
                 return true;
             default: return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void enableBluetooth(){
+        if(bluetoothAdapter.isEnabled()){
+            Toast.makeText(this, "Bluetooth is enabled", Toast.LENGTH_SHORT).show();
+        }
+        else
+            bluetoothAdapter.enable();
     }
 }
